ef7bd915e3bfd8525358a2741d366af74687721c
SAI RAM PRAGNEY MURIKIPUDI <sairampragney@gmail.com>
2026-09-19 16:41:22 +0530

    Add Java_Loops_1.java for multiplication table

---
+++ Java Loops 1/Java_Loops_1.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class Java_Loops_1 {
+  
+    public static void main(String[] args){
+      
+        Scanner sc = new Scanner(System.in);
+      
+        int a = sc.nextInt();
+      
+        for (int b = 1; b <= 10; b++){
+          
+            System.out.println(a +" x " + b + " = " + (a * b));
+          
+        }
+      
+    }
+  
+}
